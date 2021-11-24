4bb0d9df58128e399d30e2a55e80463a4e87eae3
ziosha <zioshadarkmetal@gmail.com>
2021-11-24 17:11:00 -0400

    update js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-let sarco = document.getElementById('sarco'),
-    crema = document.getElementById('crema');
+const nicho = document.getElementById('nichos');
 
     
+
 const apiGetDescEmp = fetch("http://localhost/cementerio-proyecto/controller/api/apiGetDescEmp.php");
 apiGetDescEmp.then (resp => { resp.json()
               .then (data =>{
@@ -23,26 +23,17 @@ apiGetNicho.then (resp =>{ resp.json()
                 const sarcofagos = data.Sarcofagos;
                 for (const x of cremados){
                   let {Nombres, ApellidoP, ApellidoM, Estado, FechaDef} = x;
-                  crema.innerHTML = crema.innerHTML + ren_nicho(Nombres, ApellidoP, ApellidoM, Estado, FechaDef);
+                  
                 }
                 for (const z of sarcofagos){
                   let {Nombres, ApellidoP, ApellidoM, Estado, FechaDef} = z;
-                  sarco.innerHTML = sarco.innerHTML + ren_sarco(Nombres, ApellidoP, ApellidoM, Estado, FechaDef);
+                
                 }
-
              })  
 })
 
 
-
-
-
-
-
-
-
-
-const ren_nicho = (nom, apP, apM, est, fech) =>{
+const ren_nicho = (nom, apP, apM, est, fech) => {
     return  ` </br><hr/> <p>Nombre: ${nom}</p>
     <p>A Paterno: ${apP}</p>
     <p>A Materno: ${apM}</p>
@@ -51,7 +42,7 @@ const ren_nicho = (nom, apP, apM, est, fech) =>{
     `
 }
 
-const ren_sarco = (nom, apP, apM, est, fech) =>{
+const ren_sarco = (nom, apP, apM, est, fech) => {
   return `  </br><hr/><p>Nombre: ${nom}</p>
   <p>A Paterno: ${apP}</p>
   <p>A Materno: ${apM}</p>
@@ -59,3 +50,36 @@ const ren_sarco = (nom, apP, apM, est, fech) =>{
   <p>Fecha: ${fech}</p>
   `
 }
+
+
+let h = "holaaaaaaaaaaaaaaaaaaaaaa"
+
+const box_nicho = () => {
+  return `  <a tabindex="0" class="btn btn-lg btn-danger" role="button" data-bs-toggle="popover" data-bs-trigger="focus" title="Dismissible popover" data-bs-content="${h}">Dismissible popover</a>
+  `
+}
+
+nicho.innerHTML = box_nicho();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
