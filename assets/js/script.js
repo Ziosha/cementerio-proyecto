c2fab07e750a84d6914e383614961b625b57a60e
ziosha <zioshadarkmetal@gmail.com>
2021-11-24 19:53:11 -0400

    js 50%

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,57 @@
-const nicho = document.getElementById('nichos');
+const bnicho = document.getElementById ('buton_nichos'),
+      rcremacion = document.getElementById ('cremacion'),
+      rsarcofago =document.getElementById ('sarcofago');
 
     
 
 const apiGetDescEmp = fetch("http://localhost/cementerio-proyecto/controller/api/apiGetDescEmp.php");
 apiGetDescEmp.then (resp => { resp.json()
               .then (data =>{
-                const desEmp = data;
+                const empleados = data;
+                console.log(empleados);
               })
 })
 
 const apiGetInfoEmp = fetch("http://localhost/cementerio-proyecto/controller/api/apiGetInfoEmp.php");
 apiGetInfoEmp.then (resp =>{ resp.json()
              .then (data =>{
-                 const infoEmp = data;
+                 const infoEmpleados = data;
+                 console.log(infoEmpleados);
              })  
 })
 
 const apiGetNicho = fetch("http://localhost/cementerio-proyecto/controller/api/apiGetNIcho.php")
 apiGetNicho.then (resp =>{ resp.json() 
              .then (data =>{ 
-                const cremados = data.Cremados;
-                const sarcofagos = data.Sarcofagos;
-                for (const x of cremados){
-                  let {Nombres, ApellidoP, ApellidoM, Estado, FechaDef} = x;
-                  
+                const {Nicho} = data;
+                for (const x in Nicho ){
+                    if(x <= 9){
+                        bnicho.innerHTML = bnicho.innerHTML + boton_nicho(x);
+                    }
                 }
-                for (const z of sarcofagos){
-                  let {Nombres, ApellidoP, ApellidoM, Estado, FechaDef} = z;
                 
+                for (const h of Nicho){
+                      let {Cremados} = h;
+                      for (const hh of Cremados){
+                          let {Nombres, ApellidoP, ApellidoM, Estado, FechaDef} = hh;
+                          rcremacion.innerHTML = rcremacion.innerHTML + ren_cremacion(Nombres, ApellidoP, ApellidoM, Estado, FechaDef)
+                            break;
+                      }
+                      
                 }
+                // console.log(Nicho[0].Cremados[0]);
+
              })  
 })
 
 
-const ren_nicho = (nom, apP, apM, est, fech) => {
-    return  ` </br><hr/> <p>Nombre: ${nom}</p>
-    <p>A Paterno: ${apP}</p>
-    <p>A Materno: ${apM}</p>
-    <p>Estado: ${est}</p>
-    <p>Fecha: ${fech}</p>
+const ren_cremacion = (nom,apP,apM,est,fec) => {
+    return  `<div> <a tabindex="0" class="btn btn-lg btn-danger" role="button" data-bs-toggle="popover" data-bs-trigger="focus" title="Dismissible popover" data-bs-content="Que onda camaradas ">Nich</a>
+    </div>
     `
 }
+
+
 
 const ren_sarco = (nom, apP, apM, est, fech) => {
   return `  </br><hr/><p>Nombre: ${nom}</p>
@@ -52,14 +63,13 @@ const ren_sarco = (nom, apP, apM, est, fech) => {
 }
 
 
-let h = "holaaaaaaaaaaaaaaaaaaaaaa"
 
-const box_nicho = () => {
-  return `  <a tabindex="0" class="btn btn-lg btn-danger" role="button" data-bs-toggle="popover" data-bs-trigger="focus" title="Dismissible popover" data-bs-content="${h}">Dismissible popover</a>
+const boton_nicho = (x) => {
+  return `  <button class="boton"  >Nicho A - ${x} </button>
   `
 }
 
-nicho.innerHTML = box_nicho();
+
 
 
 
