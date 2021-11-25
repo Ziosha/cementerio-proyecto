b1fd6328bf25eb9116f7515d7832da3e691e67dd
ziosha <zioshadarkmetal@gmail.com>
2021-11-25 10:39:44 -0400

    app js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const bnicho = document.getElementById ('buton_nichos'),
       rcremacion = document.getElementById ('cremacion'),
-      rsarcofago =document.getElementById ('sarcofago');
+      rsarcofago =document.getElementById ('sarcofago'),
+      colornicho = document.getElementById ('nichos_rend');
 
     
 
@@ -23,49 +24,157 @@ apiGetInfoEmp.then (resp =>{ resp.json()
 const apiGetNicho = fetch("http://localhost/cementerio-proyecto/controller/api/apiGetNIcho.php")
 apiGetNicho.then (resp =>{ resp.json() 
              .then (data =>{ 
-                const {Nicho} = data;
+                const {Nicho} = data; console.log(Nicho);
                 for (const x in Nicho ){
                     if(x <= 9){
                         bnicho.innerHTML = bnicho.innerHTML + boton_nicho(x);
                     }
                 }
-                
-                for (const h of Nicho){
-                      let {Cremados} = h;
-                      for (const hh of Cremados){
-                          let {Nombres, ApellidoP, ApellidoM, Estado, FechaDef} = hh;
-                          rcremacion.innerHTML = rcremacion.innerHTML + ren_cremacion(Nombres, ApellidoP, ApellidoM, Estado, FechaDef)
-                            break;
-                      }
-                      
-                }
-                // console.log(Nicho[0].Cremados[0]);
+            })  
+        })
+        
+const ren_nicho = (x) => {
+    const apiGetNichos = fetch("http://localhost/cementerio-proyecto/controller/api/apiGetNIcho.php")
+    apiGetNichos.then (r =>{r.json()
+                .then(dat => {
+                    const {Nicho} = dat
+                    console.log(Nicho[x]); 
+                    let {Cremados} = Nicho[x];
+                    let {Sarcofagos} = Nicho[x];
+                    if (Nicho[x].Estado == "Comprado" || Nicho[x].Estado == "Rentado"){
+                            rcremacion.innerHTML = " ";
+                            rsarcofago.innerHTML = " ";
+                            setTimeout( () =>{
+                                for (const hh of Cremados){
+                                    let {Nombres, ApellidoP, ApellidoM, Estado, FechaDef} = hh;
+                                    rcremacion.innerHTML = rcremacion.innerHTML + ren_cremacionV(Nombres, ApellidoP, ApellidoM, Estado, FechaDef);
+                                }
+                                for (const xx of Sarcofagos){
+                                    let {Nombres, ApellidoP, ApellidoM, Estado, FechaDef} = xx;
+                                    rsarcofago.innerHTML= rsarcofago.innerHTML + ren_sarcofagoV(Nombres, ApellidoP, ApellidoM, Estado, FechaDef);
+                                }
+                            },300)
+                        console.log(Nicho[x].Estado);
+                    } else {
+                        rcremacion.innerHTML = " ";
+                        rsarcofago.innerHTML = " ";
+                        setTimeout( () =>{
+                            for (const hh in Cremados){
+                                rcremacion.innerHTML = rcremacion.innerHTML + ren_cremacionR("Disponible Banda");
+                            }
+                            for (const xx of Sarcofagos){
+                                rsarcofago.innerHTML= rsarcofago.innerHTML + ren_sarcofagoR("Disponible Cumpita");
+                            }
+                        },300)
+                        console.log("Orale a comoprar putito");
+                    }
 
-             })  
-})
+
+                }) 
+            }) .catch(console.warn);
+ }
 
 
-const ren_cremacion = (nom,apP,apM,est,fec) => {
-    return  `<div> <a tabindex="0" class="btn btn-lg btn-danger" role="button" data-bs-toggle="popover" data-bs-trigger="focus" title="Dismissible popover" data-bs-content="Que onda camaradas ">Nich</a>
-    </div>
+    
+
+
+
+const ren_cremacionV = (nom,apP,apM,est,fec) => {
+    colornicho.style="box-shadow: 0 0 30px rgba(241, 7, 7, 0.61), inset 0 0 1px rgba(255, 255, 255, 0.055);"
+    return  `	<div class="serv animate__animated animate__fadeInUp">
+	    	<div class="col" ontouchstart="this.classList.toggle('hover');" >
+				<div class="container">
+		    		<div class="front" style="background-image: url(assets/imagenes/crema.jpg); box-shadow: 0 0 30px rgba(241, 7, 7, 0.61), inset 0 0 1px rgba(255, 255, 255, 0.055);" >
+		            	<div class="inner" >
+			            
+		             		<span>1</span>
+		             	</div>
+		             </div>
+		            <div class="back" style="box-shadow: 0 0 30px rgba(241, 7, 7, 0.61), inset 0 0 1px rgba(255, 255, 255, 0.055);">
+		            	<div class="inner">
+		                	<a href="#" style="color: aliceblue; text-decoration: none;" ><p style=" font-zise='12px'">Nombre:${nom} Apellidos: ${apP}${apM} Estado: ${est}  Fecha: ${fec} </p></a>
+		            	</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+    `
+}
+const ren_sarcofagoV = (nom,apP,apM,est,fec) => {
+    colornicho.style="box-shadow: 0 0 30px rgba(241, 7, 7, 0.61), inset 0 0 1px rgba(255, 255, 255, 0.055);"
+    return  `	<div class="serv animate__animated animate__fadeInUp" >
+	    	<div class="col" ontouchstart="this.classList.toggle('hover');" >
+				<div class="container">
+		    		<div class="front" style="background-image: url(assets/imagenes/Sarcofago.jpg); box-shadow: 0 0 30px rgba(241, 7, 7, 0.61), inset 0 0 1px rgba(255, 255, 255, 0.055);">
+		            	<div class="inner">
+			            
+		             		<span>1</span>
+		             	</div>
+		             </div>
+		            <div class="back" style="box-shadow: 0 0 30px rgba(241, 7, 7, 0.61), inset 0 0 1px rgba(255, 255, 255, 0.055);">
+		            	<div class="inner">
+		                	<a href="#" style="color: aliceblue; text-decoration: none;" ><p style=" font-zise='12px'">Nombre:${nom} Apellidos: ${apP}${apM} Estado: ${est}  Fecha: ${fec} </p></a>
+		            	</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+    `
+}
+
+const ren_cremacionR = (dispo) => {
+    colornicho.style="box-shadow: 0 0 30px rgba(15, 241, 7, 0.719), inset 0 0 1px rgba(255, 255, 255, 0.055);"
+    return  `	<div class="serv animate__animated animate__fadeInUp">
+	    	<div class="col" ontouchstart="this.classList.toggle('hover');">
+				<div class="container">
+		    		<div class="front" style="background-image: url(assets/imagenes/crema.jpg); box-shadow: 0 0 30px rgba(15, 241, 7, 0.719), inset 0 0 1px rgba(255, 255, 255, 0.055);">
+		            	<div class="inner">
+			            
+		             		<span>1</span>
+		             	</div>
+		             </div>
+		            <div class="back" style="box-shadow: 0 0 30px rgba(15, 241, 7, 0.719), inset 0 0 1px rgba(255, 255, 255, 0.055);">
+		            	<div class="inner">
+		                	<a href="#" style="color: aliceblue; text-decoration: none;" ><p style=" font-zise='12px'">${dispo} </p></a>
+		            	</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+    `
+}
+const ren_sarcofagoR = (dispo) => {
+    colornicho.style="box-shadow: 0 0 30px rgba(15, 241, 7, 0.719), inset 0 0 1px rgba(255, 255, 255, 0.055);"
+    return  `	<div class="serv animate__animated animate__fadeInUp">
+	    	<div class="col" ontouchstart="this.classList.toggle('hover');">
+				<div class="container">
+		    		<div class="front" style="background-image: url(assets/imagenes/crema.jpg); box-shadow: 0 0 30px rgba(15, 241, 7, 0.719), inset 0 0 1px rgba(255, 255, 255, 0.055);">
+		            	<div class="inner">
+			            
+		             		<span>1</span>
+		             	</div>
+		             </div>
+		            <div class="back" style="box-shadow: 0 0 30px rgba(15, 241, 7, 0.719), inset 0 0 1px rgba(255, 255, 255, 0.055);">
+		            	<div class="inner">
+		                	<a href="#" style="color: aliceblue; text-decoration: none;" ><p style=" font-zise='12px'">${dispo} </p></a>
+		            	</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	
     `
 }
 
 
 
-const ren_sarco = (nom, apP, apM, est, fech) => {
-  return `  </br><hr/><p>Nombre: ${nom}</p>
-  <p>A Paterno: ${apP}</p>
-  <p>A Materno: ${apM}</p>
-  <p>Estado: ${est}</p>
-  <p>Fecha: ${fech}</p>
-  `
-}
-
 
 
 const boton_nicho = (x) => {
-  return `  <button class="boton"  >Nicho A - ${x} </button>
+  return `  <button class="boton"  onclick="ren_nicho(${x})"  >Nicho A - ${x} </button>
   `
 }
 
